cb6af33f57b12c93b4f02e220adfd50a40de739d
Icia_cr <icia83@gmail.com>
2018-01-02 00:30:46 +0100

    create function for menu header

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ document.getElementById( "xButton" )
 	.addEventListener( "click", function() {
 		this.classList.toggle( "active" );
 });
+
+//open menu header
+function openClose() {
+  if (navMenu.style.display == 'block'){
+		navMenu.style.display ='none';
+	} else {
+		navMenu.style.display = 'block';
+	}
+}
+var navMenu = document.getElementById( "navigation" );
+navMenu.addEventListener('click', openClose);
+
+//open second menu header
+function openMenu(evt) {
+	evt.stopPropagation();
+	evt.currentTarget.childNodes[1].style.display = 'block'
+}
+var secondMenu = document.getElementsByClassName('menu_item');
+for (var i = 0; i < secondMenu.length; i++) secondMenu[i].addEventListener('click', openMenu);
