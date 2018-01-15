13ebdfcf63ea494f4005e86ce34e4bd936b44542
mluena <maluenarod@gmail.com>
2018-01-15 22:11:11 +0100

    test results

--- js/test.js
+++ js/test.js
@@ -3,30 +3,36 @@
 
 var testResult = document.querySelectorAll('.test_input');
 var resultado = document.querySelector('.btn_result');
+var resultadoFinal = document.querySelector('.resultado');
+var a = 0;
+
 resultado.addEventListener('click', checkTest);
 function checkTest(){
-	var a = 0;
 	for (var j=0; j<testResult.length; j++){
-
-	if (testResult[j].value == 0 && testResult[j].checked){
-		a = a++;
+		if (testResult[j].value == 0 && testResult[j].checked){
+			a++;
+		}
+		else if (testResult[j].value == 1 && testResult[j].checked) {
+			a = a+2;
+		}
+		else if (testResult[j].value == 2 && testResult[j].checked) {
+			a = a+3;
+		}
 	}
-	else if (testResult[j].value == 1 && testResult[j].checked) {
-		a = a+2;
-	}
-	else if (testResult[j].value == 2 && testResult[j].checked) {
-		a = a+3;
-	}
-
-
+	mostrarMensaje();
 }
-}
-if (a < 11){
-	prompt('Machista');
+
+function mostrarMensaje(){
+if (a > 0 && a < 11){
+	resultadoFinal.innerHTML = 'eres machista';
 }
 else if (a >= 11 && a <=20) {
-	prompt('Machista a mejorar');
+	resultadoFinal.innerHTML = 'no eres machista';
+}
+else if (a > 20){
+	resultadoFinal.innerHTML = 'me da igual si eres machista';
 }
 else {
-	prompt('no Machista');
+
+}
 }
